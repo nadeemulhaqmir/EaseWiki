4338301e8a2af51eb577e5c147bc1fe1f6703bda
mohansai <mohansaikrishna.m20@iiits.in>
2023-12-16 17:55:54 +0530

    Restructured the response of related articles API

--- backend/controllers/WikiController.js
+++ backend/controllers/WikiController.js
@@ -4,7 +4,7 @@ exports.getArticleData = ( async (req, res, next) => {
     const query = req.params.query;
     try { 
       const response = await axios.get(`https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=${query}&formatversion=2&exsentences=10&exlimit=1&explaintext=1`);
-      const data = response.data;
+      const data = response.data.query.pages[0].extract;
       res.json(data);
     } catch (error) {
       console.error('Error fetching Wikipedia article:', error);
@@ -17,7 +17,30 @@ exports.getRelatedArticles = ( async (req, res, next) => {
     try {
       const response = await axios.get(`https://en.wikipedia.org/w/api.php?action=opensearch&search=${query}&format=json`);
       const data = response.data;
-      res.json(data);
+      
+      const titles = response.data[1];
+      const links = response.data[3];
+      const length = titles.length;
+      
+      const jsonObject = {
+        count: length,
+        articles: []
+      };
+      
+      
+      // Populate the JSON object with titles and corresponding links
+      for (let i = 0; i < length; i++) {
+        const item = {
+          title: titles[i],
+          link: links[i]
+        };
+        jsonObject.articles.push(item);
+      }
+      
+
+
+      res.json(jsonObject);
+
     } catch (error) {
       console.error('Error fetching Wikipedia article:', error);
       res.status(500).json({ error: 'Internal Server Error' });
